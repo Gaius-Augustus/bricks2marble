1044b742a999171927e96c2b2ad0c62726623ef2
irkri <irkri@irkri.net>
2026-02-09 11:15:36 +0100

    new comparison plot on multiple tools

--- bricks2marble/tools/compare.py
+++ bricks2marble/tools/compare.py
@@ -4,7 +4,7 @@ import string
 import subprocess
 from collections.abc import Sequence
 from pathlib import Path
-from typing import overload
+from typing import overload, Literal
 
 import numpy as np
 import plotly.express as px
@@ -575,5 +575,204 @@ def plot_comparison(
             zerolinewidth=2,
             zerolinecolor='black',
         ),
+    )
+    return fig
+
+
+def plot_comparison_changes(
+    metrics: Sequence[Sequence[AnnotationComparison]],
+    label_species: Sequence[str] | None = None,
+    label_tools: Sequence[str] | None = None,
+    zoom: bool = True,
+    flip_axes: bool = False,
+    f1_curve: bool = True,
+    include: list[Literal[
+        "base", "intron", "transcript", "exon", "intron_chain", "locus"
+    ]] | None = None,
+) -> go.Figure:
+    keys = include if include is not None else [
+        "base", "intron", "transcript", "exon", "intron_chain", "locus"
+    ]
+    fig = make_subplots(
+        rows=1,
+        cols=len(keys),
+        subplot_titles=[
+            key[0].upper()+key[1:].replace("_", "-") for key in keys
+        ],
+        specs=[len(keys) * [{'type': 'scatter'}]],
+        horizontal_spacing=0.05,
+    )
+    if label_species is None:
+        label_species = [f"Species {i+1}" for i in range(len(metrics))]
+    if label_tools is None:
+        label_tools = [f"Model {i+1}" for i in range(len(metrics[0]))]
+
+    SymbolValidator = ValidatorCache.get_validator("scatter.marker", "symbol")
+    raw_symbols = SymbolValidator.values[2:19*12:12]
+    while len(metrics) > len(raw_symbols):
+        raw_symbols += SymbolValidator.values[2:19*12:12]
+    colors = px.colors.qualitative.Plotly
+    while len(metrics) > len(colors):
+        colors += px.colors.qualitative.Plotly
+
+    bnd = {}
+    # bnds[key] = [sens lower, sens upper, prec lower, prec upper]
+    for k, group in enumerate(metrics):
+        for i, metric in enumerate(group):
+            for j, key in enumerate(keys):
+                if k == 0 and i == 0:
+                    bnd[key] = [
+                        getattr(metric, key).sensitivity,
+                        getattr(metric, key).sensitivity,
+                        getattr(metric, key).precision,
+                        getattr(metric, key).precision,
+                    ]
+                else:
+                    bnd[key] = [
+                        min(bnd.get(key)[0], getattr(metric, key).sensitivity),
+                        max(bnd.get(key)[1], getattr(metric, key).sensitivity),
+                        min(bnd.get(key)[2], getattr(metric, key).precision),
+                        max(bnd.get(key)[3], getattr(metric, key).precision),
+                    ]
+                row = 1
+                col = j + 1
+                x, y, f1 = (
+                    getattr(metric, key).sensitivity,
+                    getattr(metric, key).precision,
+                    getattr(metric, key).F1,
+                )
+                if flip_axes:
+                    x, y = y, x
+
+                hover_text = [
+                    f"{label_species[k]}<br>{label_tools[i]}"
+                    f"<br>Sensitivity: {x:.3f}<br>Precision: {y:.3f}"
+                    f"<br>F1: {f1:.3f}"
+                ]
+                fig.add_trace(go.Scatter(
+                    x=[x],
+                    y=[y],
+                    name=f"{label_species[k]} - {label_tools[i]}",
+                    marker=dict(
+                        symbol=raw_symbols[i],
+                        color=colors[k],
+                        size=8,
+                    ),
+                    showlegend=False,
+                    legendgroup=f"group {k}",
+                    text=hover_text,
+                    hoverinfo="text"
+                ), row=row, col=col)
+
+                if f1_curve:
+                    P = np.linspace(f1/2, 1, 100)
+                    denom = 2*P - f1
+                    R = np.full_like(P, np.nan)
+                    valid = denom > 0
+                    R[valid] = f1 * P[valid] / denom[valid]
+                    inside = (P >= 0) & (P <= 1) & (R >= 0) & (R <= 1)
+                    fig.add_trace(go.Scatter(
+                        x=P[inside],
+                        y=R[inside],
+                        name=f"{label_species[k]} - {label_tools[i]}",
+                        mode="lines",
+                        line=dict(
+                            color=colors[k],
+                            dash="dot",
+                            width=1,
+                        ),
+                        legendgroup=f"group {k}",
+                        hoverinfo="skip",
+                        showlegend=False,
+                    ), row=row, col=col)
+
+    for k, group in enumerate(metrics):
+        fig.add_trace(go.Scatter(
+            x=[None], y=[None],
+            name=label_species[k],
+            mode="lines",
+            line=dict(color=colors[k], width=12),
+            showlegend=True,
+            legendgroup=f"group",
+            legendgrouptitle_text="Species",
+        ), row=row, col=col)
+    for i, metric in enumerate(metrics[0]):
+        fig.add_trace(go.Scatter(
+            x=[None], y=[None],
+            name=label_tools[i],
+            mode="markers",
+            marker=dict(
+                symbol=raw_symbols[i],
+                color="black",
+                size=12,
+            ),
+            showlegend=True,
+            legendgroup=f"metric",
+            legendgrouptitle_text="Tool",
+        ), row=row, col=col)
+
+    for j, key in enumerate(keys):
+        row = 1
+        col = j + 1
+        bound_x = [
+            max(0, bnd[key][0 if not flip_axes else 2]-0.1),
+            min(1, bnd[key][1 if not flip_axes else 3]+0.1),
+        ]
+        bound_y = [
+            max(0, bnd[key][2 if not flip_axes else 0]-0.1),
+            min(1, bnd[key][3 if not flip_axes else 1]+0.1),
+        ]
+        bounds = [min(bound_x[0], bound_y[0]), max(bound_x[1], bound_y[1])]
+        fig.update_xaxes(
+            range=[0, 1] if not zoom else bounds,
+            scaleanchor=f'y{j+1}',
+            constrain='domain',
+            row=row,
+            col=col,
+            dtick=.1 if not zoom else None,
+            minor=dict(
+                dtick=0.02,
+                showgrid=True,
+                gridcolor="rgba(0,0,0,0.05)",
+            ),
+        )
+        fig.update_yaxes(
+            range=[0, 1] if not zoom else bounds,
+            constrain='domain',
+            row=row,
+            col=col,
+            dtick=.1 if not zoom else None,
+            minor=dict(
+                dtick=0.02,
+                showgrid=True,
+                gridcolor="rgba(0,0,0,0.05)",
+            ),
+        )
+        axis_id = "" if j == 0 else str(j+1)
+        fig.add_annotation(
+            text="Sensitivity" if not flip_axes else "Precision",
+            xref=f"x{axis_id} domain", yref=f"y{axis_id} domain",
+            x=1.0, y=0,
+            showarrow=False,
+            font=dict(size=14),
+            opacity=0.5,
+            xanchor="right", yanchor="bottom",
+        )
+        fig.add_annotation(
+            text="Precision" if not flip_axes else "Sensitivity",
+            xref=f"x{axis_id} domain", yref=f"y{axis_id} domain",
+            x=0, y=1.0,
+            showarrow=False,
+            font=dict(size=14),
+            opacity=0.5,
+            textangle=-90,
+            xanchor="left", yanchor="top",
+        )
+
+    fig.update_layout(
+        width=len(keys)*300+300,
+        height=300,
+        margin=dict(l=30, r=0, t=30, b=30),
+        title=dict(xref="container", yref="container", yanchor="bottom"),
     )
     return fig
